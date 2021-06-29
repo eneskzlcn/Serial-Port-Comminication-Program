858b0272f1e58df93c9dd0ff9d45a29d3220df5d
ORIGIN <enes.kizilcin@gmail.com>
2021-06-29 13:16:49 +0300

    project files initialized and project scope in defined

---
+++ SerialPortProgram/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SerialPortTerminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AvailablePortsBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SendingMessageBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReceivedStrings = new System.Windows.Forms.ListBox();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.ControlBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AvailablePortsBox
+            // 
+            this.AvailablePortsBox.FormattingEnabled = true;
+            this.AvailablePortsBox.Location = new System.Drawing.Point(42, 60);
+            this.AvailablePortsBox.Name = "AvailablePortsBox";
+            this.AvailablePortsBox.Size = new System.Drawing.Size(121, 24);
+            this.AvailablePortsBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Available Ports";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(68, 102);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(85, 23);
+            this.ConnectButton.TabIndex = 3;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(659, 323);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(89, 46);
+            this.SendButton.TabIndex = 5;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // SendingMessageBox
+            // 
+            this.SendingMessageBox.Location = new System.Drawing.Point(287, 335);
+            this.SendingMessageBox.Name = "SendingMessageBox";
+            this.SendingMessageBox.Size = new System.Drawing.Size(344, 22);
+            this.SendingMessageBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Command";
+            // 
+            // ReceivedStrings
+            // 
+            this.ReceivedStrings.FormattingEnabled = true;
+            this.ReceivedStrings.ItemHeight = 16;
+            this.ReceivedStrings.Items.AddRange(new object[] {
+            "asasf",
+            "asf",
+            "saf",
+            "fas",
+            "sfa",
+            "fas"});
+            this.ReceivedStrings.Location = new System.Drawing.Point(333, 60);
+            this.ReceivedStrings.Name = "ReceivedStrings";
+            this.ReceivedStrings.Size = new System.Drawing.Size(271, 196);
+            this.ReceivedStrings.TabIndex = 8;
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(68, 140);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(95, 33);
+            this.DisconnectButton.TabIndex = 9;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // ControlBox
+            // 
+            this.ControlBox.FormattingEnabled = true;
+            this.ControlBox.ItemHeight = 16;
+            this.ControlBox.Location = new System.Drawing.Point(43, 254);
+            this.ControlBox.Name = "ControlBox";
+            this.ControlBox.Size = new System.Drawing.Size(120, 84);
+            this.ControlBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 478);
+            this.Controls.Add(this.ControlBox);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ReceivedStrings);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendingMessageBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AvailablePortsBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox AvailablePortsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox SendingMessageBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ReceivedStrings;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.ListBox ControlBox;
+    }
+}
+
---
+++ SerialPortProgram/Form1.cs
@@ -0,0 +1,111 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.IO.Ports;
+    using System.IO;
+
+    namespace SerialPortTerminal
+    {
+        public partial class Form1 : Form
+        {
+            private SerialPort serialPort;
+        
+            
+            public Form1()
+            {
+                InitializeComponent();
+                getAvailablePorts();
+                ReceivedStrings.Items.Clear();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+            public void getAvailablePorts()
+            {
+                String[] ports = SerialPort.GetPortNames();
+                AvailablePortsBox.Items.AddRange(ports);
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                serialPort = new SerialPort();
+                serialPort.PortName = AvailablePortsBox.Text;
+                serialPort.BaudRate = 9600;
+                serialPort.Parity = Parity.None;
+                serialPort.DataBits = 8;
+                serialPort.StopBits = StopBits.One;
+                serialPort.ReadBufferSize = 10000;
+           
+                serialPort.DataReceived += OnDataReceivedOnSerialPort;
+
+                try
+                {
+                    if(!serialPort.IsOpen)
+                        serialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "...Error");
+                }
+           
+            
+            }
+            private void OnDataReceivedOnSerialPort(object sender, SerialDataReceivedEventArgs e)
+            {
+                
+                string message = serialPort.ReadLine();
+                ReceivedStrings.Items.Add(message);
+
+            }
+            private void SendButton_Click(object sender, EventArgs e)
+            {
+                serialPort.WriteLine(SendingMessageBox.Text.ToString());
+            }
+        
+            private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                try
+                {
+                    serialPort.Close();
+                }
+                catch(Exception ex){
+
+                }
+            }
+
+            private void DisconnectButton_Click(object sender, EventArgs e)
+            {
+                try
+                { 
+                    serialPort.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error when disconnetting from port!");
+                    if (Application.MessageLoop)
+                    {
+                        // WinForms app
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        // Console app
+                        Environment.Exit(1);
+                    }
+                }
+                MessageBox.Show("Disconnected From Port!");
+            }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
